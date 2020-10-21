f5ec4acf4cf8e1142b50f99b9962e0cf1ca2d0f0
unknown <tunarshreal@gmail.com>
2020-10-21 15:00:21 -0200

    Testing the Life Cycle

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import About from './About'
 import Home from './Home'
 import Items from './Items'
+import Apsp from './LifeCycle'
+import Pick from './Pick'
 import Services from './Services'
 
 
@@ -9,24 +11,32 @@ export class Index extends Component {
     render() {
         return (
             <div>
-              <h1>
-                    i am the header tag here
-                </h1>
                 <nav className="nav">
                     <ul className="list" style={thisStyle}>
                         <li className="list-item">  <Home/> </li>
                         <li className="list-item">  <Items/> </li>
                         <li className="list-item">  <Services /> </li>
                         <li className="list-item">  <About/></li>
+                        <li className="list-item">  <Pick/></li>
                     </ul>
+                    <form style={formStyle}>
+                        <input type="search" placeholder="Search Your Favorite"/>
+                        <input type="submit" placeholder="Search "/>
+                    </form>
                 </nav>
+                <Apsp />
             </div>
         )
     }
 }
 const thisStyle={
     display:"flex",
-    // backgroundColor:"purple",
+    float:"right",
+    marginRight:"15%",
+}
+const formStyle={
+    margin:"auto",
+    marginTop:"1%",
 }
 
 export default Index
