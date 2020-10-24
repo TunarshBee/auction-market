800806f0ba3b72e2d73708f008a1b51997a15e4e
unknown <tunarshreal@gmail.com>
2020-10-24 15:14:12 -0200

    the Carousel Component

---
+++ src/components/Carousel.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Carousel from "react-elastic-carousel";
+import asset1 from "./image/asset1.jpg";
+import asset6 from "./image/asset6.jpg";
+import asset7 from "./image/asset7.png";
+import asset8 from "./image/asset8.jpg";
+import asset5 from "./image/asset5.jpg";
+
+class Slider extends Component {
+  state = {
+    items: [
+      { id: 1, image: asset1 },
+      { id: 2, image: asset6 },
+      { id: 3, image: asset7 },
+      { id: 4, image: asset8 },
+      { id: 5, image: asset5 },
+    ],
+  };
+
+  render() {
+    const { items } = this.state;
+    // console.log(items[0].image)
+    return (
+      <Carousel  style={caro}>
+          {items.map((item) => (
+            <div key={item.id}>
+              <img src={item.image} style={imgStyle} className="caroImg" />{" "}
+            </div>
+          ))}
+      </Carousel>
+    );
+  }
+}
+
+const imgStyle = {
+  margin: "0%",
+  padding: "0%",
+  marginTop: "5%",
+  height:"170px",
+  width: "100%",
+};
+const caro = {
+  margin: "0%",
+    float:"right",
+  padding: "0%",
+  width: "50%",
+};
+
+export default Slider;
--- src/components/Index.js
+++ src/components/Index.js
@@ -1,48 +1,81 @@
-import React, { Component } from 'react'
-import About from './About'
-import Home from './Home'
-import Items from './Items'
-import Pick from './Pick'
-import ComputerAssessories from './ComputerAssessories'
-import Services from './Services'
-import Fashion from './Fashion'
-import PhonesAndTablets from './PhonesAndTablets'
-import Games from './Games'
-
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+import About from "./About";
+import Home from "./Home";
+import Items from "./Items";
+import Pick from "./Pick";
+import ComputerAssessories from "./ComputerAssessories";
+import Services from "./Services";
+import Fashion from "./Fashion";
+import PhonesAndTablets from "./PhonesAndTablets";
+import Games from "./Games";
+import Carousel from "./Carousel";
 
 export class Index extends Component {
-    render() {
-        return (
-            <div>
-                <nav className="nav">
-                    <ul className="list" style={thisStyle}>
-                        <li className="list-item">  <Home/> </li>
-                        <li className="list-item">  <Items/> </li>
-                        <li className="list-item">  <Services /> </li>
-                        <li className="list-item">  <About/></li>
-                        <li className="list-item">  <Pick/></li>
-                    </ul>
-                    <form style={formStyle}>
-                        <input type="search" placeholder="Search Your Favorite"/>
-                        <input type="submit" placeholder="Search "/>
-                    </form>
-                </nav>
-                    <ComputerAssessories/>
-                    <PhonesAndTablets/>
-                    <Fashion/>
-                    <Games/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="nav">
+          <ul className="list" style={thisStyle}>
+            <li className="list-item">
+              {" "}
+              <Home />{" "}
+            </li>
+            <li className="list-item">
+              {" "}
+              <Items />{" "}
+            </li>
+            <li className="list-item">
+              {" "}
+              <Services />{" "}
+            </li>
+            <li className="list-item">
+              {" "}
+              <About />
+            </li>
+            <li className="list-item">
+              {" "}
+              <Pick />
+            </li>
+          </ul>
+          <form style={formStyle}>
+            <input type="search" placeholder="Search Your Favorite" />
+            <input type="submit" placeholder="Search " />
+          </form>
+        </nav>
+        <div className="itemStyle">
+          <ComputerAssessories className="style" />
+          <PhonesAndTablets className="style" />
+          <Fashion className="style" />
+          <Games className="style" />
+          {/* <ComputerAssessories className="style" />
+          <PhonesAndTablets className="style" /> */}
+          <Fashion className="style" />
+          <Games className="style" />
+        </div>
+        <Carousel className="carousel" />
+        <div style={style1Frag}>
+            <ComputerAssessories className="style1" />
+            <PhonesAndTablets className="style1" />
+            <Fashion className="style1" />
+            <Games className="style1" />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-const thisStyle={
-    display:"flex",
-    float:"right",
-    marginRight:"15%",
-}
-const formStyle={
-    margin:"auto",
-    marginTop:"1%",
+const thisStyle = {
+  display: "flex",
+  float: "right",
+  marginRight: "15%",
+};
+const formStyle = {
+  margin: "auto",
+  // clear:"both",
+  marginTop: "1%",
+};
+const style1Frag={
+    color:"#fff",
 }
 
-export default Index
+export default Index;
